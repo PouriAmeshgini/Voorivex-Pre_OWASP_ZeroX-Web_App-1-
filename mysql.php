0241abc7433daff009303e66acf361b5bc8fdd9c
Pouria Meshgini <103781052+PouriAmeshgini@users.noreply.github.com>
2026-06-16 01:03:37 +0330

    Delete mysql.php

--- mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$username = 'dbuser';
-$password = 'dbpassword';
-$database = 'VooriMedDB';
-$connection = new mysqli($host, $username, $password, $database);
-if ($connection->connect_error) {
-    die("Connection Failed: " . $connection->connect_error);
-}
